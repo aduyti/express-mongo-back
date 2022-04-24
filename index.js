c6167778670e84eb1948f5a8add30b97b12c32cc
Anirudhya <anirudhyaduyti@gmail.com>
2022-04-25 04:05:37 +0600

    db connect

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// mango
+// user: dbmangotest
+// pass: Y8wvuoNezPsTst0M
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2bong.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("db connected ");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World! nodemon');
